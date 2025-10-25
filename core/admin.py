666f228f833c3a12d638621f8759dc840e4dd3ff
Oxbee59 <agyeiosborn59@gmail.com>
2025-10-25 14:35:55 +0000

    Fixed admin login template error and restricted to staff users

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,7 @@ def custom_admin_login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=admin, password=Admin@12345)
 
         if user and user.is_staff:
             login(request, user)
