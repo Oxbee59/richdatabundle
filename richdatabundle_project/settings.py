2e6cfe0b6cfb14eff7d47f9cc2f4e3ab1e16da18
Oxbee59 <agyeiosborn59@gmail.com>
2025-10-30 03:00:33 +0000

    Fix login redirect issue

--- richdatabundle_project/settings.py
+++ richdatabundle_project/settings.py
@@ -114,6 +114,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # -----------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # App user login/signup
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
